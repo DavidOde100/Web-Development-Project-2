2943d1454c6d6e2313670ed296c34d5b9ce7a3f5
David <david.odejimi677@gmail.com>
2023-10-01 16:27:15 -0500

    add

--- info-quiz/src/QuizCard.jsx
+++ info-quiz/src/QuizCard.jsx
@@ -1,9 +1,11 @@
-// QuizCard.jsx
 import React, { useState } from 'react';
 import '/src/QuizCard.css';
 
 const QuizCard = ({ question, answer, image, category, onNextClick }) => {
   const [showAnswer, setShowAnswer] = useState(false);
+  const [userGuess, setUserGuess] = useState(''); // To track user input
+  const [isCorrect, setIsCorrect] = useState(null); // To track correctness
+  const [submitted, setSubmitted] = useState(false); // To track submission
 
   const toggleAnswer = () => {
     setShowAnswer(!showAnswer);
@@ -23,18 +25,45 @@ const QuizCard = ({ question, answer, image, category, onNextClick }) => {
     }
   };
 
+  const handleInputChange = (e) => {
+    setUserGuess(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Check if the user's guess matches the answer
+    const isGuessCorrect = userGuess.toLowerCase() === answer.toLowerCase();
+    setIsCorrect(isGuessCorrect);
+    setSubmitted(true);
+  };
+
   return (
     <div className={`quiz-card ${getCategoryClassName()}`} onClick={toggleAnswer}>
       {showAnswer ? (
         <>
           <h3>Answer:</h3>
           <p>{answer}</p>
+          {submitted && (
+            <p className={isCorrect ? 'correct' : 'incorrect'}>
+              {isCorrect ? 'Correct!' : 'Incorrect!'}
+            </p>
+          )}
         </>
       ) : (
         <>
           <h3>Question:</h3>
           <p>{question}</p>
           {image && <img src={image} alt="Card Image" />}
+          {!submitted && (
+            <div>
+              <input
+                type="text"
+                placeholder="Enter your guess"
+                value={userGuess}
+                onChange={handleInputChange}
+              />
+              <button onClick={handleSubmit}>Submit</button>
+            </div>
+          )}
         </>
       )}
       <button onClick={onNextClick}>Next</button>
@@ -43,3 +72,4 @@ const QuizCard = ({ question, answer, image, category, onNextClick }) => {
 };
 
 export default QuizCard;
+
